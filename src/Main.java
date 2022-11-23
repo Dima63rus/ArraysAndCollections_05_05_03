7ff8ac242477b99e667947a17350e3af627a067d
Dima <dmitriy.matveev89@inbox.ru>
2022-11-23 10:13:32 +0300

    Ver 2.0

--- src/Main.java
+++ src/Main.java
@@ -10,8 +10,26 @@ public class Main {
       x
      x x
     x   x
-   x     x 
+   x     x
 */
+        char[][] ltFigure = {
+                {'X', ' ', ' ', ' ', ' ', ' ', 'X' },
+                {' ', 'X', ' ', ' ', ' ', 'X', ' ' },
+                {' ', ' ', 'X', ' ', 'X', ' ', ' ' },
+                {' ', ' ', ' ', 'X', ' ', ' ', ' ' },
+                {' ', ' ', 'X', ' ', 'X', ' ', ' ' },
+                {' ', 'X', ' ', ' ', ' ', 'X', ' ' },
+                {'X', ' ', ' ', ' ', ' ', ' ', 'X' },
+        };
 
+        for (int i = 0; i < ltFigure.length; i++) {
+            for (int j = 0; j < ltFigure[i].length; j++) {
+                if (j == ltFigure[i].length - 1) {
+                    System.out.println(ltFigure[i][j]);
+                } else {
+                    System.out.print(ltFigure[i][j]);
+                }
+            }
+        }
     }
 }
